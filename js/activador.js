a5b70e973b1890d7534a031cc22fa45bd4972c95
Francisco Rotundo <francorotundo90@gmail.com>
2022-06-17 20:31:46 -0400

    Se colocó el video de presentación y se mejoró el boton de contacto

--- js/activador.js
+++ js/activador.js
@@ -2,7 +2,7 @@ particlesJS(
     {
         "particles": {
           "number": {
-            "value": 100,
+            "value": 60,
             "density": {
               "enable": true,
               "value_area": 800
